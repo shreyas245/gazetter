f6b4929f24aed841c78bcc8379f45febe5a6b3e7
Ionut Parvu <74410385+ionutparvu85@users.noreply.github.com>
2021-10-07 15:27:46 +0100

    update the URL of restcountries

--- libs/php/getCountryInfo.php
+++ libs/php/getCountryInfo.php
@@ -1,6 +1,6 @@
 <?php
 
-	$url='https://restcountries.eu/rest/v2/alpha/' . $_REQUEST['code'];
+	$url='https://restcountries.com/v2/alpha/' . $_REQUEST['code'];
 	
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
